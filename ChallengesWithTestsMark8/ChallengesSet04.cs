0c412105ca17433088b3d17a190e486c57de99d4
Brett Lazarine <jblazarine@gmail.com>
2023-02-27 01:05:11 -0600

    complete weekly challenges set 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,62 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            var oddIsNegative = numbers.Where(x => x % 2 != 0).Select(x => -x);
+            var even = numbers.Where(x =>x % 2 == 0);
+            return even.Sum() + oddIsNegative.Sum();
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            var lst = new List<string> { str1, str2, str3, str4 };
+            return lst.Min(x => x.Length);
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            return new List<int> { number1, number2, number3,number4 }.Min(x => x);
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+            return sideLength1 + sideLength2 > sideLength3 && sideLength1 + sideLength3 > sideLength2 && sideLength2 + sideLength3 > sideLength1;
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            bool isNum;
+            double notNeeded;
+            isNum = double.TryParse(input, out notNeeded);
+            return isNum;
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            var countNull = objs.Where(x => x == null).Count();
+            return countNull > objs.Count() / 2;
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null) { return 0; }
+            var evens = numbers.Where(x => x % 2 == 0);
+            return evens.Count() != 0 ? evens.Average() : 0;
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+            if (number == 0) { return 1; }
+            if (number < 0) { throw new ArgumentOutOfRangeException(); }
+            for (int i = number - 1; i > 0; i--)
+            {
+                number *= i;
+            }
+            return number;
         }
     }
 }
