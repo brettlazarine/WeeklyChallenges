cfae14a947d9736565590b2ab04e14dc0f8cfa6e
Brett Lazarine <jblazarine@gmail.com>
2023-03-13 21:04:58 -0500

    complete weekly challenges set 6

--- ChallengesWithTestsMark8/ChallengesSet06.cs
+++ ChallengesWithTestsMark8/ChallengesSet06.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,27 +8,104 @@ namespace ChallengesWithTestsMark8
     {
         public bool CollectionContainsWord(IEnumerable<string> words, string word, bool ignoreCase)
         {
-            throw new NotImplementedException();
+            if (words == null || words.Contains(null)) return false; //null check
+            var match = false;
+            if (ignoreCase)
+            {
+                foreach (var search in words)
+                {
+                    if (search.ToLower() == word.ToLower())
+                    {
+                        match = true;
+                        break;
+                    }
+                    match = false;
+                }
+            }
+            if (!ignoreCase)
+            {
+                foreach (var search in words)
+                {
+                    if (search == word)
+                    {
+                        match = true;
+                        break;
+                    }
+                    match = false;
+
+                }
+            }
+            return match;
         }
 
         public bool IsPrimeNumber(int num)
         {
-            throw new NotImplementedException();
+            if (num < 2) return false;
+            if (num == 2) return true;
+            for (int i = 2; i <= Math.Sqrt(num); i++)
+            {
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
         }
 
         public int IndexOfLastUniqueLetter(string str)
         {
-            throw new NotImplementedException();
+            if (str.Length == 1) return 0;
+            if (str.Length == 0 || str.Distinct().Count() == 1) return -1;
+            var index = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if ((str.Count(x => x == str[i]) == 1)) //save last index of unique char
+                {
+                    index = i;
+                }
+            }
+            return index;
         }
 
         public int MaxConsecutiveCount(int[] numbers)
         {
-            throw new NotImplementedException();
+            var most = 0;
+            for (int i = 0; i <= numbers.Length - 1; i++)
+            {
+                int count = 1;
+                for (int j =  i + 1; j <= numbers.Length - 1; j++)
+                {
+                    if (numbers[i] != numbers[j])
+                    {
+                        break;
+                    }
+                    count++;                       
+                }
+                if (count > most)
+                {
+                    most = count;
+                }
+            }
+            return most;
         }
 
         public double[] GetEveryNthElement(List<double> elements, int n)
         {
-            throw new NotImplementedException();
+            if (elements == null || elements.Count == 0 || n <= 0)
+            {
+                return new double[0];
+            }
+            List<double> result = new();
+            for (int i = 0; i < elements.Count;)
+            {
+                i += n;
+                if (i > elements.Count)
+                {
+                    break;
+                }
+                result.Add(elements[i - 1]);
+            }
+            return result.ToArray();
         }
     }
 }
